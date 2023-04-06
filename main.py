2b026511794dabf49a03fde2c3156eccc127682b
sets018 <91557108+sets018@users.noreply.github.com>
2023-04-05 20:34:20 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+import streamlit as st
+st.set_page_config(page_title="Streamlit Geospatial", layout="wide")
+
+st.title(' Laboratorio 2 - Estructura de datos 2')
+
+# A dictionary of apps in the format of {"App title": "App icon"}
+# More icons can be found here: https://icons.getbootstrap.com
